cc86821617edd1b750ac8c6fe52bcc7eda07237a
ParangBird <seekerman@naver.com>
2021-12-03 16:45:04 +0900

    first commit

---
+++ app/src/main/java/com/example/sangrok_delivery/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sangrok_delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/sangrok_delivery/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.sangrok_delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView ani;
+        ani = findViewById(R.id.imageView2);
+        Animation anim1 = AnimationUtils.loadAnimation(this, R.anim.activity_splash_imageview);
+        ani.setAnimation(anim1);
+
+        TextView text;
+        text = findViewById(R.id.textView);
+        Animation anim2 = AnimationUtils.loadAnimation(this, R.anim.activity_textanim);
+        text.setAnimation(anim2);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new splashHandler(), 3000);
+        // 3초 뒤 메인화면 진입
+    }
+
+    private class splashHandler implements Runnable {
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));        // LoginActivity 로 이동
+            finish();  // SplashActivity 종료
+        }
+    }
+}
